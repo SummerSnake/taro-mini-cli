c9410d8943ee74024e2210bbab0817e65afd2d87
liyan <33661226lll@163.com>
2020-09-08 16:41:08 +0800

    feat: 首页列表

--- src/pages/basicLayout/components/Home/index.js
+++ src/pages/basicLayout/components/Home/index.js
@@ -1,7 +1,7 @@
 import Taro, { useState, useEffect } from '@tarojs/taro';
 import { View, Text, Image, Swiper, SwiperItem, ScrollView } from '@tarojs/components';
 import getSystemInfo from '@/utils/getSystemInfo';
-import { isObj, verifyArr } from '@/utils/util';
+import { isObj, verifyArr, formatNumberToRMB } from '@/utils/util';
 
 import './index.scss';
 
@@ -17,6 +17,13 @@ function Home() {
   const noticeList = [
     { id: 1, title: 'SummerSnake' },
     { id: 2, title: 'ECMAScript' },
+  ];
+
+  const cardList = [
+    { id: 1, title: 'ECMAScript', num: 90, money: 10000, txt: '门前大桥下' },
+    { id: 2, title: 'React', num: 80, money: 10000, txt: '门前大桥下' },
+    { id: 3, title: 'Taro', num: 70, money: 10000, txt: '门前大桥下' },
+    { id: 4, title: 'Webpack', num: 60, money: 10000, txt: '门前大桥下' },
   ];
 
   /**
@@ -53,7 +60,7 @@ function Home() {
             circular
             autoplay
             indicator-dots
-            indicator-active-color="#fd5f10"
+            indicator-active-color="#2083e4"
             className="bannerWrap"
           >
             {verifyArr(bannerList) &&
@@ -86,6 +93,55 @@ function Home() {
                   </SwiperItem>
                 ))}
             </Swiper>
+          </View>
+
+          {/* 列表 */}
+          <View className="listWrap">
+            {verifyArr(cardList) &&
+              cardList.map((item) => (
+                <View key={item.id} className="cardDom">
+                  <View className="cardLeft">
+                    <Image className="cardLogo" src="https://s1.ax1x.com/2020/08/31/dO5leU.jpg" />
+                  </View>
+                  <View className="cardRight">
+                    <View className="cardTitle">
+                      <Text>{item.title}</Text>
+                      <Text
+                        className="circleSign"
+                        style={{
+                          backgroundColor:
+                            item.num >= 90
+                              ? '#009A61'
+                              : item.num >= 80 && item.num < 90
+                              ? '#2083e4'
+                              : item.num >= 70 && item.num < 80
+                              ? '#fb7e48'
+                              : '#e80e27',
+                        }}
+                      >
+                        {item.num >= 90
+                          ? '中'
+                          : item.num >= 80 && item.num < 90
+                          ? '国'
+                          : item.num >= 70 && item.num < 80
+                          ? '加'
+                          : '油'}
+                      </Text>
+                    </View>
+                    <View className="cardTxt ellipsis">文字：{item.txt}</View>
+                    <View className="cardTxt">
+                      文字：{item.txt}
+                      <Text>文字：{item.txt}</Text>
+                    </View>
+                    <View className="cardTxt">文字：{item.txt}</View>
+                    <View className="cardDate">
+                      文字：{item.txt}
+                      <Text>文字：{item.txt}</Text>
+                    </View>
+                    <View className="moneyDom">￥{formatNumberToRMB(item.money)}</View>
+                  </View>
+                </View>
+              ))}
           </View>
         </View>
       </ScrollView>
